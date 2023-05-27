5d6293bdc5aafa462adbd33e21fda2e0b3ecb688
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2023-05-28 02:10:12 +0900

    05. 도커를 이용한 간단한 Node.js 어플 만들기

--- practice/05_nodejs-docker-app/server.js
+++ practice/05_nodejs-docker-app/server.js
@@ -6,9 +6,9 @@ const PORT = 8080; // Express 서버를 위한 포트 설정
 // App
 const app = express(); // 새로운 Express 어플 생성
 app.get('/', (req, res)=>{ // '/' 경로로 요청이 오면 Hello World를 결과값으로 전달.
-  res.send('Hello World!');
+  res.send('Hello World! Using Volume!!! Using PWD!');
 });
 
 // app.listen(PORT, HOST); // 해당 포트와 호스트에서 HTTP 서버를 시작
 app.listen(PORT); // 해당 포트와 호스트에서 HTTP 서버를 시작
-// console.log(`Running on http://${HOST}: ${PORT}`);
+console.log(`Server is running!`);
